ebfdbad8810f32c276a242ddae34a504363843ab
Chris Larson <c.lars701@gmail.com>
2018-09-07 17:10:49 -0700

    add first test for circleci

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+// app.use(express.static(), 'public');
+// app.use(express.json());
+
+// app.get('/hello', (req, res) => {
+//   res.status(200).json(req);
+// });
+
+const testFunction = input => input;
+
+// app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
+
+module.exports = testFunction;
