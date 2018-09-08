3ef84c2c05056d0d566664597314f91a76aa3e35
Chris Larson <c.lars701@gmail.com>
2018-09-07 18:33:24 -0700

    Add babel and webpack config

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// const app = express();
-// app.use(express.static(), 'public');
-// app.use(express.json());
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
 
-// app.get('/hello', (req, res) => {
-//   res.status(200).json(req);
-// });
+app.get('/hello', (req, res) => {
+  res.status(200).json(req);
+});
 
 const testFunction = input => input;
 
-// app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
 
 module.exports = testFunction;
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.jsx$/, exclude: /node_modules/, use: 'babel-loader' },
+    ],
+  },
+};
