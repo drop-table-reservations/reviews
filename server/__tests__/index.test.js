306429d98628d5a9414564f672a43a72f39d3dd1
Chris Larson <c.lars701@gmail.com>
2018-09-07 17:32:20 -0700

    add sample test to configure circleci

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -3,3 +3,7 @@ const testFunction = require('../index');
 test('testFunction returns the input its given', () => {
   expect(testFunction('hello CircleCI')).toBe('hello CircleCI');
 });
+
+test('testFunction returns the input its given again', () => {
+  expect(testFunction('hello CircleCI2')).toBe('hello CircleCI2');
+});
