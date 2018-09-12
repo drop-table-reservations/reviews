410c27e34780397af97e2ee308eef3decde4da81
Chris Larson <c.lars701@gmail.com>
2018-09-12 10:50:47 -0700

    add required props to List test

--- src/client/components/__tests__/List.test.jsx
+++ src/client/components/__tests__/List.test.jsx
@@ -3,12 +3,13 @@ import { shallow, mount } from 'enzyme';
 import List from '../List';
 
 describe('List component', () => {
+  const reviewsToDisplay = [{ _id: 12345, rating: 5 }];
 
   test('it should be selectable by id "lis"', () => {
-    expect(shallow(<List />).is('#list')).toBe(true);
+    expect(shallow(<List reviewsToDisplay={reviewsToDisplay}/>).is('#list')).toBe(true);
   });
 
   test('it should mount in a full DOM', () => {
-    expect(mount(<List />).find('#list').length).toBe(1);
+    expect(mount(<List reviewsToDisplay={reviewsToDisplay}/>).find('#list').length).toBe(1);
   });
 });
