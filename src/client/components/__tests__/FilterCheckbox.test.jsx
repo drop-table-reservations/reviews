025f75a5426607c2869057e414962dfcddf0917d
Chris Larson <c.lars701@gmail.com>
2018-09-17 16:03:48 -0700

    mvp filter panel

--- src/client/components/__tests__/FilterCheckbox.test.jsx
+++ src/client/components/__tests__/FilterCheckbox.test.jsx
@@ -1,5 +1,65 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FilterCheckbox from '../FilterCheckbox';
+
+const mockFilterCategories = jest.fn((targetCategory) => targetCategory);
+
+/*
+  <FilterCheckbox
+    label={category}
+    filterCategories={filterCategories}
+    checked={categories.has(category)}
+  />
+*/
+
 describe('FilterCheckbox component', () => {
-  test('True to be true', () => {
-    expect(true).toBe(true);
+  test('should render icon and paragraph', () => {
+    const wrapper = shallow(
+      <FilterCheckbox
+        label="Good for Groups"
+        filterCategories={mockFilterCategories}
+        checked
+      />,
+    );
+    expect(wrapper.find('FilterCheckbox__Icon')).toHaveLength(1);
+    expect(wrapper.find('p')).toHaveLength(1);
+  });
+
+  test('should render checked icon if checked is true', () => {
+    const wrapper = shallow(
+      <FilterCheckbox
+        label="Good for Groups"
+        filterCategories={mockFilterCategories}
+        checked
+      />,
+    );
+    expect(wrapper.find('FilterCheckbox__Icon').html()).toContain('fa-check-square-o');
+  });
+
+  test('should render unchecked icon if checked is false', () => {
+    const wrapper = shallow(
+      <FilterCheckbox
+        label="Good for Groups"
+        filterCategories={mockFilterCategories}
+        checked={false}
+      />,
+    );
+    expect(wrapper.find('FilterCheckbox__Icon').html()).toContain('fa-square-o');
+  });
+
+  test('should invoke filterCategories with the dataset value of label', () => {
+    const wrapper = shallow(
+      <FilterCheckbox
+        label="Good"
+        filterCategories={mockFilterCategories}
+        checked
+      />,
+    );
+    expect(mockFilterCategories.mock.calls).toHaveLength(0);
+    wrapper
+      .find('FilterCheckbox__Container')
+      .simulate('click', { currentTarget: { dataset: { value: 'Good' } } });
+    expect(mockFilterCategories.mock.calls).toHaveLength(1);
+    expect(mockFilterCategories.mock.calls[0][0]).toBe('Good');
   });
 });
